bc941554e00095142a6bd0acc3df56983b7729ab
pal12-coder <73381341+pal12-coder@users.noreply.github.com>
2020-11-29 01:13:24 +0530

    Add files via upload

---
+++ Paperball.js
@@ -0,0 +1,23 @@
+class PaperBall {
+  constructor(x, y) {
+    var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    this.image = loadImage("paper.png");
+    this.body = Bodies.circle(x, y,30,options);
+    this.radius=30;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    push();
+    translate(pos.x, pos.y);
+   imageMode(CENTER);
+    image(this.image, 0, 0, this.radius*2, this.radius*2);
+    fill("red");
+    pop();
+  }
+};
